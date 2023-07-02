2416be9348ed83917161991f9408b5fd1c135069
Dawid Konopiaty <darkspine.33@gmail.com>
2023-07-02 14:54:06 +0200

    Optimalize Code AI

--- Fast-Click/script.js
+++ Fast-Click/script.js
@@ -1,22 +1,21 @@
 const containerObject = document.getElementById("container-object");
 
-const maxWidht = containerObject.offsetWidth;
-//game seting
+const maxWidth = containerObject.offsetWidth;
 const sec = 19;
 const mobileOB = 3;
-const PcOB = 7;
-const numberOfObject = maxWidht > 1000 ? PcOB : mobileOB;
+const pcOB = 7;
+const numberOfObject = maxWidth > 1000 ? pcOB : mobileOB;
 const maxPoint = numberOfObject;
 const maxHeight = containerObject.offsetHeight;
 
 // Number of objects
 for (let i = 1; i <= numberOfObject; i++) {
   const newObject = document.createElement("button");
-  newObject.draggable = "true";
+  newObject.draggable = true;
   newObject.textContent = "";
   newObject.id = "object-" + i;
   newObject.className = "object";
-  newObject.tabIndex = "-1";
+  newObject.tabIndex = -1;
   newObject.type = "text";
   newObject.addEventListener("click", () => {
     dp(i);
@@ -33,30 +32,30 @@ for (let i = 1; i <= numberOfObject; i++) {
 function Rposition() {
   for (let i = 1; i <= maxPoint; i++) {
     const object = document.getElementById("object-" + i);
-    const maxValue = maxWidht;
+    const maxValue = maxWidth;
     const minValue = 0;
     const math = Math.floor(Math.random() * (maxValue - minValue));
     object.style.left = math + "px";
 
-    const maxValueH = maxHeight;
-    const mathH = Math.floor(Math.random() * (maxValueH - minValue));
+    const mathH = Math.floor(Math.random() * maxHeight);
     object.style.top = mathH + "px";
   }
 }
 Rposition();
 window.addEventListener("resize", Rposition);
-// After click respown
+
+// After click respawn
 function dp(n) {
   const object = document.getElementById("object-" + n);
-  const maxValue = maxWidht;
+  const maxValue = maxWidth;
   const minValue = 0;
   const math = Math.floor(Math.random() * (maxValue - minValue));
   object.style.left = math + "px";
 
-  const maxValueH = maxHeight;
-  const mathH = Math.floor(Math.random() * (maxValueH - minValue));
+  const mathH = Math.floor(Math.random() * maxHeight);
   object.style.top = mathH + "px";
 }
+
 // Counter points
 let counter = 0;
 const point = document.getElementById("point");
@@ -65,23 +64,19 @@ const result = document.getElementById("result");
 function up() {
   counter++;
   point.textContent = counter;
-  result.innerHTML = "Congratulation Your Score Is: <div>" + counter + "</div>";
+  result.innerHTML = `Congratulation Your Score Is: <div>${counter}</div>`;
 
   if (counter >= 25) {
     point.style.color = "yellow";
-
-    document
-      .getElementById("point-wraper")
-      .addEventListener("mouseover", () => {
-        point.style.opacity = "20%";
-        point.style.color = "red";
-      });
-    document
-      .getElementById("point-wraper")
-      .addEventListener("mouseleave", () => {
-        point.style.opacity = "100%";
-        point.style.color = "yellow";
-      });
+    const pointWraper = document.getElementById("point-wraper");
+    pointWraper.addEventListener("mouseover", () => {
+      point.style.opacity = "20%";
+      point.style.color = "red";
+    });
+    pointWraper.addEventListener("mouseleave", () => {
+      point.style.opacity = "100%";
+      point.style.color = "yellow";
+    });
   }
 
   if (counter >= 50) {
@@ -95,13 +90,11 @@ function rel() {
 }
 
 // Timer
-// document.addEventListener("click", function (event) {
-//   if (event.target.classList.contains("object")) {
 let seconds = sec;
 const timer = document.getElementById("timer");
 setTimeout(() => {
   const interval = setInterval(() => {
-    timer.innerHTML = seconds + "s";
+    timer.textContent = `${seconds}s`;
     seconds--;
 
     if (seconds === -1) {
@@ -111,7 +104,3 @@ setTimeout(() => {
     }
   }, 1000);
 }, 1300);
-
-// document.removeEventListener("click", arguments.callee);
-//   }
-// });
